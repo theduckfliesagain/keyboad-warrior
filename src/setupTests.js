63302e7cd910d79540cb57431166348cbdce4c61
Rishi <rishi_bhugobaun@yahoo.co.uk>
2021-06-02 10:30:28 +0100

    add globals to test setup

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,12 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+import React from 'react';
+import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import '@testing-library/jest-dom/extend-expect';
+
+global.React = React;
+global.render = render;
+global.userEvent = userEvent;
