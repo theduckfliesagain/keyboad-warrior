3bdd28e31f4884f64e184a5c1c3444821b9ae3c2
Rishi <rishi_bhugobaun@yahoo.co.uk>
2021-06-02 11:10:18 +0100

    render game page in app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import * as Pages from './pages' 
 
 function App() {
   return (
     <div className="App">
-      
+      <Pages.Game />
     </div>
   );
 }
