abf10c2f5a697ce2ec175f2d3c22f33a4c7158aa
Rishi <rishi_bhugobaun@yahoo.co.uk>
2021-06-07 13:57:47 +0100

    add complete function to update focus to next word

--- src/components/Game/WordBlock.js
+++ src/components/Game/WordBlock.js
@@ -2,6 +2,14 @@ import React from 'react'
 import Word from './Word';
 
     const wordRefs = useRef([])
+    const complete = (idx) => {
+        if (idx < words.length - 1) {
+            wordRefs.current[idx + 1].focus()    
+        } else {
+            console.log('DONE');
+        }
+    }
+
     const words = wordBlock.split(' ').map((word, idx) => (
         <Word 
             key={idx} id={idx} word={word} complete={complete} 
