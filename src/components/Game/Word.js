6e32045773ea25d037a6df30b703b1959389f406
Rishi <rishi_bhugobaun@yahoo.co.uk>
2021-06-07 13:49:38 +0100

    add correct state

--- src/components/Game/Word.js
+++ src/components/Game/Word.js
@@ -1,4 +1,6 @@
     const [input, setInput] = useState('');
+    const [correct, setCorrect] = useState(false);
+
     const handleInput = (e) => {
         if ((word).startsWith(e.target.value)) setInput(e.target.value)
     }
