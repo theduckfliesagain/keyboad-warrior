2308041544a59366c8fc2014114283cf55b62b7e
Rishi <rishi_bhugobaun@yahoo.co.uk>
2021-06-07 14:01:36 +0100

    trigger complete using key down event

--- src/components/Game/Word.js
+++ src/components/Game/Word.js
@@ -1,3 +1,8 @@
+/* eslint-disable no-unused-vars */
+import React, { useState, useEffect } from 'react'
+import './Word.scss'
+
+export default function Word({ id, word, complete, wordRef }) {
     const [input, setInput] = useState('');
     const [correct, setCorrect] = useState(false);
 
@@ -8,11 +13,14 @@
     const handleComplete = (e) => {
         if(word === input && e.code === 'Space') complete(id)
     }
+
     useEffect(() => {
         setCorrect(word.startsWith(input))
     }, [input])
+
     return (
         <div className={`Word ${correct ? 'correct' : ''}`}>
+            <input type="text" value={input} onChange={handleInput} onKeyDown={handleComplete}
             style={{width: word.length + 'ch'}} tabIndex={-1} ref={wordRef}/>
             <span>{word}</span>
         </div>
