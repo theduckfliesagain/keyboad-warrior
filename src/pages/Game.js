789b841bca7227e863601e4d05b023943dd40a14
Rishi <rishi_bhugobaun@yahoo.co.uk>
2021-06-09 11:34:59 +0100

    add function to update score and accuracy stats

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -2,6 +2,17 @@ import React from 'react'
 import { WordBlock } from '../components/Game'
 
 export default function Game() {
+    const [stats, setStats] = useState({score: 0, accuracy: []});
+    const baseScore = 10;
+
+    const updateStats = (word) => {
+        setStats(stats => {
+            return {
+                score: stats.score += baseScore * word.accuracy,
+                accuracy: [...stats.accuracy, word.accuracy] 
+            }
+        })
+    }
 
     const wordBlock = "These are the words you have to type."
 
