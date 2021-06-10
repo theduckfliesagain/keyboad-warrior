50df8ef5ed21c051bc4c480047eb79a8d175b13d
Rishi <rishi_bhugobaun@yahoo.co.uk>
2021-06-10 11:58:27 +0100

    add interactions tests

--- src/components/Game/WordBlock.test.js
+++ src/components/Game/WordBlock.test.js
@@ -1,14 +1,39 @@
-import { screen } from '@testing-library/react';
+import { fireEvent, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import WordBlock from './WordBlock'
 
 describe('WordBlock', () => {
     describe('rendering', () => {
         beforeEach(() => {
-            render(<WordBlock wordBlock={"Test word block"}/>)
+            render(<WordBlock wordBlock={"Test word block"} />)
         });
 
-        it.each(["Test", "word", "block"])('render %s', (word) => {
+        it.each(["Test", "word", "block"])('should render %s', (word) => {
             expect(screen.getByText(word)).toBeInTheDocument()
+        });
+    });
+
+    describe('interaction', () => {
+        let wordBlock;
+        let updateStats;
+
+        beforeEach(() => {
+            updateStats = jest.fn();
+            wordBlock = render(<WordBlock wordBlock={"Test typing these words."} updateStats={updateStats} />)
+        });
+
+        it('should be focused on the first input on load', () => {
+            expect(wordBlock.getAllByRole('textbox')[0]).toHaveFocus()
+        });
+
+        it('should shift focus to the next word when the user presses space and the word is complete', () => {
+            const firstWord = wordBlock.getAllByRole('textbox')[0];
+
+            userEvent.type(firstWord, "Test", { skipClick: true })
+            expect(wordBlock.getAllByRole('textbox')[0]).toHaveValue('Test')
+
+            fireEvent.keyDown(firstWord, { key: 'Space', code: 'Space' })
+            expect(wordBlock.getAllByRole('textbox')[1]).toHaveFocus()
         });
     });
 });
