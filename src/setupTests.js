2e21defb2f6c84ac00ec706d1d6dc0c5f8b08798
Rishi <rishi_bhugobaun@yahoo.co.uk>
2021-06-10 11:59:15 +0100

    remove global userevent

--- src/setupTests.js
+++ src/setupTests.js
@@ -6,9 +6,7 @@ import '@testing-library/jest-dom';
 
 import React from 'react';
 import { render } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
 import '@testing-library/jest-dom/extend-expect';
 
 global.React = React;
 global.render = render;
-global.userEvent = userEvent;
